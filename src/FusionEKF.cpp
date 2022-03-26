a20f818f59d88373d5d42a9c3a7aae0c201bc946
Atilla Özdemir <81968553+Atilla1976@users.noreply.github.com>
2022-03-26 08:57:06 +0100

    Update FusionEKF.cpp

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -160,13 +160,13 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
     Tools tools;
-    efk_.H_ = tools.CalulateJacobian(ekf_.x_);
+    Hj_ = tools.CalulateJacobian(ekf_.x_);
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
 
   } else {
     // TODO: Laser updates
-    ekf_.H_ = H_laser_;
+    Hj_ = H_laser_;
     ekf_.R_ = R_laser_;
     ekf_.Update(measurement_pack.raw_measurements_);
   }
